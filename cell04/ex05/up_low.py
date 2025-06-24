42b6fcf24f17b79edd492af4aae932136740ebc9
hamnuttapat1 <67011213@kmitl.ac.th>
2025-06-24 21:32:59 +0700

    ex01

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,8 @@
+word = input("Give me a word: ")
+for i in word:
+    if i.isupper():
+        print(i.lower(), end="")
+    elif i.islower():
+        print(i.upper(), end="")
+    else:
+        print(i, end="")
