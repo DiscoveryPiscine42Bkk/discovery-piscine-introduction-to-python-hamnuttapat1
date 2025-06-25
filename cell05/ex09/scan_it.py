ebc9f6452434d87949249f8da9187df9a2cfbc08
hamnuttapat1 <67011213@kmitl.ac.th>
2025-06-25 14:54:14 +0700

    update

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -2,8 +2,8 @@
 import sys
 
 if len(sys.argv) == 3:
-    find = sys.argv[2] 
-    word = sys.argv[3] 
+    find = sys.argv[1] 
+    word = sys.argv[2] 
     count = word.count(find)
     if count == 0:
         print("none")
