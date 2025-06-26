caef5d6d5e696bb806e592ee082f47357aec7beb
hamnuttapat1 <67011213@kmitl.ac.th>
2025-06-26 15:48:02 +0700

    Update

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+import sys
+
+def shrink(eight):
+    print(eight[:8])
+def enlarge(eight):
+    if len(eight) < 8:
+        z = 8-len(eight)
+        eight += 'Z'*z
+    print(eight)
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for j in sys.argv[1:]:
+        if len(j) > 8:
+            shrink(j)
+        elif len(j) < 8:
+            enlarge(j)
+        else:
+            print(j)
+
